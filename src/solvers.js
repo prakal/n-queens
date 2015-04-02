7bfbe7ce7e4eddfcf874a955368f3db0541d8109
prakal <kalvapal@ualberta.ca>
2015-04-01 20:01:15 -0700

    Add recursive function for n rooks

--- src/solvers.js
+++ src/solvers.js
@@ -12,8 +12,8 @@
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
-
-window.findNRooksSolution = function(n, index) {
+/*
+window.findNRooksSolution = function(n, index, columnIndex) {
   var solution = new Board({n:n});
   var matrix = solution.rows();
   index = index || 0;
@@ -29,7 +29,7 @@ window.findNRooksSolution = function(n, index) {
       {
         matrix[i][count] = 1;
         anyRookInRow = true;
-        j = currentRow.length;
+
       }
       count = (count + 1) % matrix.length;
     }
@@ -41,26 +41,58 @@ window.findNRooksSolution = function(n, index) {
   solution = new Board(matrix); //fixme
   return solution;
 };
+*/
+window.findNRooksSolution = function(n, matrix, collections) {
 
+};
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = 0; //fixme
-  var allSolutions = [];
+  var collections = [];
+  var board = new Board({"n":n});
+  console.log("1");
+  var innerFunction = function(row, matrix) {
+    var newMatrix = [];
+    for (var a = 0; a < matrix.length; a++) {
+        newMatrix[a] = matrix[a].slice();
+    }
+
+    var newBoard = new Board(newMatrix);
+    console.log("2");
+    for(var i = 0; i < n ; i++)
+    {
+      console.log("3: " + i + " " + row + " " + n);
+      if(!newBoard.hasColConflictAt(i))
+      {
+        console.log("4");
+        newBoard.togglePiece(row, i);
+        if(row === n - 1)
+        {
+          console.log("6");
+          console.log(newMatrix);
+          collections.push(newBoard);
+
+        }
+        else {
+          console.log("7");
 
 
-  for (var a = 0; a < n; a++){
-    var solution = findNRooksSolution(n,a);
-    console.dir(solution);
-    if (solution !== null){
-      console.log("Add solution");
-      allSolutions.push(solution);
+          console.log("8");
+          innerFunction(row + 1, newBoard.rows());
+        }
+      }
+      else
+      {
+        console.log("hit nothing");
+      }
     }
   }
+  console.log("5");
+  innerFunction(0, board.rows());
 
-  console.log('Number of solutions for ' + n + ' rooks:', allSolutions.length);
-  return allSolutions.length;
+  console.log('Number of solutions for ' + n + ' rooks:', collections.length);
+  return collections.length;
 };
 
 
